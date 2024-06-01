7becc760df61510529a65b21d733cde18ea3ab7e
Rafael Soares <rafael6297@gmail.com>
2024-06-01 18:03:43 -0300

    Adição do envio de email e criptografia

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import mysql.connector
 import datetime
+import bcrypt
+import smtplib
+from email.mime.text import MIMEText
+import random
 
 mydb = mysql.connector.connect(
   host="uom.h.filess.io",
@@ -14,7 +18,51 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "welcome!"
+    return {"message": "Welcome!"}
+
+@app.post("/users")
+async def insert_user(nome: str, nome_usuario: str, email: str, senha: str):
+    mycursor = mydb.cursor()
+    query = "SELECT * FROM usuario WHERE email = %s OR nome_usuario = %s"
+    mycursor.execute(query, (email, nome_usuario))
+    linha = mycursor.fetchall()
+    if not linha:
+        hashed_password = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+        hashed_password_str = hashed_password.decode('utf-8')
+        verification_code = random.randint(100000, 999999)
+        query = "INSERT INTO usuario(nome, nome_usuario, email, senha, papel, status, data_criacao, codigo_verificacao) VALUES(%s, %s, %s, %s, 'usuario', 'aguardando confirmacao', %s, %s)"
+        mycursor.execute(query, (nome, nome_usuario, email, hashed_password_str, datetime.datetime.now(), verification_code))
+        mydb.commit()
+
+
+        servidor_email = smtplib.SMTP('smtp.gmail.com', 587)
+        servidor_email.starttls()
+        servidor_email.login('studynestapp@gmail.com', 'fhyh vikm mpph yhdl')
+        remetente = 'studynestapp@gmail.com'
+        destinatario = [email]
+        conteudo = (f'Olá, seja bem vindo(a)!\n'
+                    f'Este é o seu código de verificação: {verification_code}.')
+
+        msg = MIMEText(conteudo, _charset='UTF-8')
+        msg['Subject'] = 'Boas vindas e código de verificação'
+        msg['From'] = remetente
+        msg['To'] = ', '.join(destinatario)
+
+        servidor_email.sendmail(remetente, destinatario, msg.as_string())
+
+        raise HTTPException(status_code=202, detail="Usuário cadastrado com sucesso")
+
+
+    for informacao in linha:
+        bd_email = informacao[2]
+        bd_nome_usuario = informacao[1]
+
+        if email == bd_email and nome_usuario == bd_nome_usuario:
+            raise HTTPException(status_code=422, detail="Email e nome de usuário ja cadastrados")
+        elif email == bd_email:
+            raise HTTPException(status_code=422, detail="Email já cadastrado")
+        else:
+            raise HTTPException(status_code=422, detail="Nome de usuário já cadastrado")
 
 
 @app.get("/users/{email}/{senha}")
@@ -24,14 +72,12 @@ async def check_users(email: str, senha: str):
     mycursor.execute(query, (email,))
     linha = mycursor.fetchall()
 
-    if linha is None:
-        return {"message": "Usuário não existe"}
+    if not linha:
+        raise HTTPException(status_code=404, detail="Usuário não existe")
 
     for informacao in linha:
         password = informacao[3]
-        if password != senha:
-            return {"message": "Senha incorreta"}
+        if not bcrypt.checkpw(senha.encode('utf-8'), password.encode('utf-8')):
+            raise HTTPException(status_code=401, detail="Senha incorreta")
 
-    return {"message": "Login bem-sucedido"}
-
-
+    raise HTTPException(status_code=202, detail="Login realizado com sucesso")
